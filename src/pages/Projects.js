7fb3d8c0ba51d8c893ebaf447520c4b38c38c60e
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-05-27 20:31:04 -0400

    put in project detail languages

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,25 +9,29 @@ export default function Projects() {
                     <a href="https://drew-e-wilson.github.io/Crypto-frontend/" target="_blank" className={styles.project_name}><img className={styles.crypto_img} src="https://imgur.com/mWZYC22.jpg"></img>
                     <h2>CryptoTalk</h2></a>
                     <h4 className={styles.project_description}>Designed to give up-to-date crypto data and be able to save your favorite cryptos for easy access to see their price, market cap, and more. </h4>
-                    <h4>software used/ icons</h4>
+                    <h4>Created with:</h4>
+                    <h4>MongoDB | Express.js | React | Node.js JavaScript | REST API | Chart.js | CSS3 HTML5</h4>
                 </div>
                 <div className={styles.project_box}>
                     <a href="https://drew-e-wilson.github.io/find-a-recipe/" target="_blank" className={styles.project_name}><img className={styles.recipe_img} src="https://imgur.com/CDAnD0X.jpg"></img>
                     <h2>Find-A-Recipe</h2></a>
                     <h4 className={styles.project_description}>Searching for your favorite recipes easily. Get ingredients and step by step instructions to make some of the best recipes you can think of.</h4>
-                    <h4>software used/ icons</h4>
+                    <h4>Created with:</h4>
+                    <h4>React | JavaScript | REST API | CSS3 | HTML5</h4>
                 </div>
                 <div className={styles.project_box}>
                     <a href="https://bunmeevang.github.io/project-3-front/#/" target="_blank" className={styles.project_name}><img className={styles.codr_img} src="https://imgur.com/Y66lcHk.jpg"></img>
                     <h2 className={styles.project_name}>Codr</h2></a>
                     <h4 className={styles.project_description}>A platform designed for people to share ideas, articles and coding issues and anyone be able to comment and post their own content.</h4>
-                    <h4>software used/ icons</h4>
+                    <h4>Created with:</h4>
+                    <h4>JavaScript | React | Django | PostgreSQL CSS3 | HTML5</h4>
                 </div>
                 <div className={styles.project_box}>
                     <a href="https://drew-e-wilson.github.io/Attack-of-the-Holidays/" target="_blank" className={styles.project_name}><img className={styles.attack_img} src="https://imgur.com/ViRXswN.jpg"></img>
                     <h2 className={styles.project_name}>Attack of the Holidays</h2></a>
                     <h4 className={styles.project_description}>A single player fighting game against 3 evil holiday villians. Each villain is set in a different location. Take down all 3 to save the world.</h4>
-                    <h4>software used/ icons</h4>
+                    <h4>Created with:</h4>
+                    <h4>JavaScript | HTML5 | CSS3</h4>
                 </div>
             </div>
         </div>
