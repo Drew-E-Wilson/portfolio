eece6bcafdadc2b85e3a678ba40865f5d84b23f3
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-05-27 20:10:36 -0400

    update About me

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
                     <img className={styles.headshot} src="https://i.imgur.com/M4p5Uwb.jpg" alr="image of Drew smiling"></img>
                     <h2>Software Developer</h2>
                     <h3>Drew Wilson</h3>
-                    <p className={styles.description}>Aspiring Software Engineer with a background in International Business. I use enjoy using the MERN stack to build applications that solve clients’ needs and make their lives easier. I bring my passion for analysis and organizational knowledge to software development and use my skills and creativity to make each project unique. I love creating work that will be used by others and feel a responsibility to create the best product I can for the best experience. </p>
+                    <p className={styles.description}>Software Engineer with a background in International Business. I use enjoy using the MERN stack to build applications that solve clients’ needs and make their lives easier. I bring my passion for analysis and organizational knowledge to software development and use my skills and creativity to make each project unique. I love creating work that will be used by others and feel a responsibility to create the best product I can for the best experience. </p>
                 </div>
             </div>
         </div>
